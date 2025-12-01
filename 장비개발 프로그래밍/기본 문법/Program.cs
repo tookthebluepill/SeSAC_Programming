f68ac0dac9e56c54765e877341852ccfbf8fe2f5
James <jameslee0131@gmail.com>
2025-12-01 20:05:35 +0900

    25-12-01

---
+++ 장비개발 프로그래밍/기본 문법/Program.cs
@@ -0,0 +1,9 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+class First
+{
+    static void Main0(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 장비개발 프로그래밍/기본 문법/출력.cs
@@ -0,0 +1,113 @@
+// 출력 방법
+// 방법1: Console 클래스의 WriteLine() 메서드 사용
+// Console.Write(출력하고_싶은_대상);
+// static void Main(string[] arg)
+// {
+//     Console.WriteLine("Hello C# Programming!");
+// }
+
+public class 출력
+{
+    static void Main0(string[] args)
+    {
+        Console.WriteLine("Hello C# Programming ..!");
+    }
+// 정수
+    static void Main1(string[] args)
+    {
+        Console.WriteLine(52);
+    }
+// 연산자
+    static void Main2(string[] args)
+    {
+        Console.WriteLine(52+273);
+    }
+// 문자열
+// \t:수평 탭, \\:역슬래시, \n:행 바꿈, \":큰 따옴표
+    static void Main3(string[] args)
+    {
+        Console.WriteLine("한빛\t아카데미");
+        Console.WriteLine("한빛\n아카데미");
+        Console.WriteLine("\"\"\"");
+    }
+
+// 문자 선택
+    static void Main4(string[] args)
+    {
+        Console.WriteLine("안녕하세요"[0]);
+        Console.WriteLine("안녕하세요"[1]);
+        Console.WriteLine("안녕하세요"[3]);
+        Console.WriteLine("안녕하세요"[100]); //예외 발생
+    }
+// 문자 덧셈 연산
+// 문자는 + 연산자를 사용해도 연결되지 않음
+    static void Main5(string[] args)
+    {
+        Console.WriteLine('가' + '나');
+    }
+// 오버플로
+// 2,147,483,647에 1을 더하면 int 자료형의 범위를 넘어버리면서 -2,147,483,648이 됨
+    static void Main6(string[] args)
+    {
+        int a = 2147483640;
+        int b = 52273;
+        Console.WriteLine(a+b);
+    }
+
+    static void Main7(string[] args)
+    {
+        int a = 2147483640;
+        int b = 52273;
+        Console.WriteLine(a+b);
+    }
+    static void Main8(string[] args)
+    {
+        uint unsignedA = 200000000;
+        uint unsignedB = 100000000;
+        Console.WriteLine(unsignedA+unsignedB);
+    }
+// unsigned 자료형
+// int 자료형은 -2147483648부터2147483647까지 나타낼 수 있음
+// 음수를 사용하지 않는다면 -2147483648부터 -1까지의 숫자가 낭비됨
+// 이런 낭비를 막고자 C#은 unsigned 자료형(부호가 없는 자료형)을 제공
+// int와 long 키워드 대신 uint와 ulong 키워드를 사용    
+    static void Main9(string[] args)
+    {
+        uint unsignedInt = 4147483647;
+        ulong unsignedLong = 11223372036854775808;
+        
+        Console.WriteLine(unsignedInt);
+        Console.WriteLine(unsignedLong);
+    }
+// 최댓값과 최솟값
+    static void Main10(string[] args)
+    {
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(int.MinValue);
+    }
+
+    static void Main11(string[] args)
+    {
+        Console.WriteLine(long.MaxValue);
+        Console.WriteLine(long.MinValue);
+    }
+
+// sizeof 연산자와 char 자료형의 크기
+    static void Main12(string[] args)
+    {
+        Console.WriteLine("int: " + sizeof(int));
+        Console.WriteLine("long: " + sizeof(long));
+        Console.WriteLine("float: " + sizeof(float));
+        Console.WriteLine("double: " + sizeof(double));
+        Console.WriteLine("char: " + sizeof(char));
+    }
+// 증감 연산자
+    static void Main(string[] args)
+    {
+        int number = 10;
+        Console.WriteLine(number);
+        Console.WriteLine(number++);
+        Console.WriteLine(number--);
+        Console.WriteLine(number);
+    }
+}
