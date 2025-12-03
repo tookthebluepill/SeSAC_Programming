a81f8a2deb566e130111353deb5b29813a98ab8d
James <jameslee0131@gmail.com>
2025-12-03 17:21:46 +0900

    25-12-03

---
+++ 장비개발 프로그래밍/기본 문법/homework1.cs
@@ -0,0 +1,15 @@
+class homework1
+{
+    static void Main(string[] args)
+    {
+        string? input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            return;
+        }
+        int sup = int.Parse(input);
+        double hwa = (sup * 9.0 / 5) + 32;
+        Console.WriteLine($"섭씨 {sup} 는 화씨 {hwa} 입니다.");
+    }
+}
+
